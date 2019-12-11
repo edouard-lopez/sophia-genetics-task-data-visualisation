5d4fd53b00646d907ea86963aa341ab8a4778ffd
Edouard Lopez <contact@edouard-lopez.com>
2019-12-11 17:45:35 +0100

    fetch data from backend

--- task/backend/app/main.py
+++ task/backend/app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from app.feeder import Feeder
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
--- task/src/App.js
+++ task/src/App.js
@@ -1,9 +1,9 @@
 import * as d3 from "d3";
 import { DateTime } from "luxon";
-import React from "react";
+import React, { Component } from "react";
 import "./assets/App.css";
-import { data, domainX } from "./assets/data";
 import UserUsageGraph from "./UserUsageGraph";
+import axios from "axios";
 
 const dateRange = (start, end) =>
   d3
@@ -21,20 +21,51 @@ const prepare = (range, data) =>
     };
   });
 
-function App() {
-  const userData = data[0];
-  const from = DateTime.fromFormat(domainX.from, "MM-yyyy");
-  const to = DateTime.fromFormat(domainX.to, "MM-yyyy");
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      domainX: {},
+      userData: []
+    };
+  }
 
-  return (
-    <div className="App">
-      <div className="App-header">
-        <div className="container">
-          <UserUsageGraph data={prepare(dateRange(from, to), userData)} />
+  async componentDidMount() {
+    this.setState({ isLoading: true });
+    try {
+      const response = await axios.get("http://localhost/domain-x");
+      this.setState({ domainX: response.data, isLoading: false });
+    } catch (error) {
+      this.setState({ error, isLoading: false });
+    }
+    try {
+      const response = await axios.get("http://localhost/user-usage");
+      this.setState({ userData: response.data, isLoading: false });
+    } catch (error) {
+      this.setState({ error, isLoading: false });
+    }
+  }
+
+  render() {
+    const { domainX, userData, isLoading } = this.state;
+    if (userData.length < 1) {
+      return <h1>Loading ...</h1>;
+    }
+
+    const from = DateTime.fromFormat(domainX.from, "MM-yyyy");
+    const to = DateTime.fromFormat(domainX.to, "MM-yyyy");
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <div className="container">
+            <UserUsageGraph data={prepare(dateRange(from, to), userData[0])} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
